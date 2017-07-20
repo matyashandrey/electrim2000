222a799474f1db7274c77b42fa3517cc2f248e98
andrii.matiash <andrii.matiash@ria.com>
2017-07-20 15:32:44 +0300

    гарантия блок

--- src/containers/landings/actionVirtus.js
+++ src/containers/landings/actionVirtus.js
@@ -143,6 +143,102 @@ class actionVirtus extends Component {
                     </div>
                 </div>
             </div>
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 className="section-heading">Сертификация качества</h2>
+                    <hr className="primary"/>
+                </div>
+                <section className="no-padding" id="portfolio">
+                    <div className="container-fluid">
+                        <div className="row no-gutter popup-gallery">
+                            <div className="col-lg-4 col-sm-6">
+                                <a href="/img/clients/test1.jpg" className="portfolio-box">
+                                    <img src="/img/clients/prez.jpg" className="img-responsive" alt=""/>
+                                    <div className="portfolio-box-caption">
+                                        <div className="portfolio-box-caption-content">
+                                            <div className="project-category text-faded">
+
+                                            </div>
+                                            <div className="project-name">
+                                                Администрация Президента Украины
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div className="col-lg-4 col-sm-6">
+                                <a href="/img/clients/test4.png" className="portfolio-box">
+                                    <img height='320px' src="/img/clients/mandarin_plaza.jpg" className="img-responsive"
+                                         alt=""/>
+                                    <div className="portfolio-box-caption">
+                                        <div className="portfolio-box-caption-content">
+                                            <div className="project-category text-faded">
+
+                                            </div>
+                                            <div className="project-name">
+                                                ТРЦ "Мандарин Плаза"
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div className="col-lg-4 col-sm-6">
+                                <a href="/img/clients/test1.jpg" className="portfolio-box">
+                                    <img src="/img/clients/nemiroff.jpg" className="img-responsive" alt=""/>
+                                    <div className="portfolio-box-caption">
+                                        <div className="portfolio-box-caption-content">
+                                            <div className="project-category text-faded">
+                                                ДП Украинская водочная компания
+                                            </div>
+                                            <div className="project-name">
+                                                "Nemiroff"
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div className="row">
+                <div className="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 className="section-heading">О нашей компаниии</h2>
+                    <hr className="primary"/>
+                </div>
+                <div className="col-lg-4">
+                    <div className="col-lg-3">
+                        <img className="img-circle"
+                             src="/img/guaranty/tools.png"
+                             alt="Generic placeholder image" width="90" height="90"/>
+                    </div>
+                    <div className="col-lg-9">
+                        <h3>Работаем с 2007 года</h3>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="col-lg-3">
+                        <img className="img-circle"
+                             src="/img/guaranty/proffesional.png"
+                             alt="Generic placeholder image" width="90" height="90"/>
+                    </div>
+                    <div className="col-lg-9">
+                        <h3>Команда проффесиналов</h3>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="col-lg-3">
+                        <img className="img-circle"
+                             src="/img/guaranty/finish.png"
+                             alt="Generic placeholder image" width="90" height="90"/>
+                    </div>
+                    <div className="col-lg-9">
+                        <h3>Реализованно более ??? поектов</h3>
+                    </div>
+                </div>
+
+
+            </div>
         </div>);
 
     }
